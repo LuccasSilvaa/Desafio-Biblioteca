be633875152f5a1d019ad2b3c054383b4d593432
¨LuccasSilva¨ <correaluccas@hotmail.com>
2024-12-15 15:54:48 -0300

    subindo atualizacao do  desafio

--- src/Pages/QueroDoar/queroDoar.jsx
+++ src/Pages/QueroDoar/queroDoar.jsx
@@ -1,12 +1,29 @@
+import livro from '../../assets/queroDoar/Vector.png'
 
+import S from "./queroDoar.module.scss";
 
-export default function queroDoar(){
-
-  return(
-    <section>
-      <h2>quero doar</h2>
-    </section>
-  )
-
-
+export default function queroDoar() {
+  return (
+    <>
+      <section>
+        <section className={S.container}>
+          <h2>
+            Por favor, preencha o formulário com suas informações e as
+            informações do Livro
+          </h2>
+          <form action="">
+            <div>
+              <img src={livro} alt="" />
+              <h3>Informações do Livro</h3>
+            </div>
+            <input type="text" placeholder="Titulo" />
+            <input type="text" placeholder="Categoria" />
+            <input type="text" placeholder="Autor" />
+            <input type="text" placeholder="Link da Imagem" />
+            <input type="submit" value="Doar" />
+          </form>
+        </section>
+      </section>
+    </>
+  );
 }
