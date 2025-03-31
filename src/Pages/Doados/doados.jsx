f77e670fcc0497dd7805fcf7ee830dcb5179c15c
LuccasSilvaa <correaluccas@hotmail.com>
2025-03-31 12:37:36 -0300

    :sparkles: feat: Adicionado GET para listar livros doados

--- src/Pages/Doados/doados.jsx
+++ src/Pages/Doados/doados.jsx
@@ -1,32 +1,33 @@
+import React from "react";
 import S from "./doados.module.scss";
 import livro from "../../assets/doados/livro.png";
+import axios from "axios";
 
 export default function doados() {
+  const [dados, setDados] = React.useState([]);
+  const pegarDados = async () => {
+    const Dados = await axios.get(
+      "https://api-livros-vnw-3gyx.onrender.com/livros"
+    );
+    setDados(Dados.data);
+  };
+  React.useEffect(() => {
+    pegarDados();
+  }, [setDados]);
+
   return (
     <section className={S.containerDoados}>
       <h2>Livros Doados</h2>
 
       <section className={S.containerCard}>
-        <article>
-          <img src={livro} alt="" />
-          <h3>O Protagonista</h3>
-          <p>Susanne Andrade</p>
-          <p>Ficção</p>
-        </article>
-
-        <article>
-          <img src={livro} alt="" />
-          <h3>O Protagonista</h3>
-          <p>Susanne Andrade</p>
-          <p>Ficção</p>
-        </article>
-
-        <article>
-          <img src={livro} alt="" />
-          <h3>O Protagonista</h3>
-          <p>Susanne Andrade</p>
-          <p>Ficção</p>
-        </article>
+        {dados.map((index) => (
+          <article>
+            <img src={index.imagem_url} alt="" />
+            <h2>{index.titulo}</h2>
+            <p>Autor: {index.autor}</p>
+            <p>Categoria: {index.categoria}</p>
+          </article>
+        ))}
       </section>
     </section>
   );
--- src/Pages/QueroDoar/queroDoar.jsx
+++ src/Pages/QueroDoar/queroDoar.jsx
@@ -1,8 +1,48 @@
-import livro from '../../assets/queroDoar/Vector.png'
-
+import livro from "../../assets/queroDoar/Vector.png";
+import React from 'react';
+import axios from "axios";
 import S from "./queroDoar.module.scss";
 
 export default function queroDoar() {
+  const [titulo, setTitulo] = React.useState("");
+  const [categoria, setCategoria] = React.useState("");
+  const [autor, setAutor] = React.useState("");
+  const [imagem_url, setImagem] = React.useState("");
+
+  const enviarDados = async() => {
+    const urlApi = "https://api-livros-vnw-3gyx.onrender.com/doar"
+
+    const dadosEnviar = {
+      titulo,
+      categoria,
+      autor,
+      imagem_url
+    }
+
+    await axios.post(urlApi,dadosEnviar)
+
+    alert("Livro Enviado!")
+
+    setTitulo("")
+    setCategoria("")
+    setAutor("")
+    setImagem("")
+  }
+
+  const capturaTitulo = (e) => {
+    setTitulo(e.target.value);
+  };
+  const capturaCategoria = (e) => {
+    setCategoria(e.target.value);
+    
+  };
+  const capturaAutor = (e) => {
+    setAutor(e.target.value);
+  };
+  const capturaImagem = (e) => {
+    setImagem(e.target.value);
+  };
+
   return (
     <>
       <section>
@@ -11,16 +51,16 @@ export default function queroDoar() {
             Por favor, preencha o formulário com suas informações e as
             informações do Livro
           </h2>
-          <form action="">
+          <form onSubmit={(e) => e.preventDefault()}>
             <div>
               <img src={livro} alt="" />
               <h3>Informações do Livro</h3>
             </div>
-            <input type="text" placeholder="Titulo" />
-            <input type="text" placeholder="Categoria" />
-            <input type="text" placeholder="Autor" />
-            <input type="text" placeholder="Link da Imagem" />
-            <input type="submit" value="Doar" />
+            <input type="text" placeholder="Titulo" onChange={capturaTitulo} value={titulo} />
+            <input type="text" placeholder="Categoria" onChange={capturaCategoria} value={categoria}/>
+            <input type="text" placeholder="Autor" onChange={capturaAutor} value={autor} />
+            <input type="text" placeholder="Link da Imagem" onChange={capturaImagem} value={imagem_url} />
+            <input type="submit" value="Doar" onClick={enviarDados} />
           </form>
         </section>
       </section>
